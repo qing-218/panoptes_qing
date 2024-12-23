3b53c08f0b41ec9b558182e57b8177cbe81e389b
qing-218 <bao.yiting@outlook.com>
2024-12-23 22:01:18 +0800

    Update firefox.ts

--- modules/firefox.ts
+++ modules/firefox.ts
@@ -1,1 +1,10 @@
+import CdpBrowserModule from '../models/CdpBrowserModule.js';
+import CertificateTypes from '../constants/CertificateTypes.js';
 
+class FirefoxModule extends CdpBrowserModule {
+    protected readonly _package: string = 'org.mozilla.firefox';
+    protected readonly _cdpPort: string = 'firefox_devtools_remote';
+    protected readonly _certType: CertificateTypes = CertificateTypes.System;
+}
+
+export default new FirefoxModule();
